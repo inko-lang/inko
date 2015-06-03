5e9d39ea36cfa4f51352b69a0d19c6919db8dc97
Yorick Peterse <yorickpeterse@gmail.com>
2015-06-03 09:35:28 +0200

    Basic docs for the instruction code.

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,3 +1,4 @@
+/// Enum containing all possible instruction types.
 #[derive(Debug, Clone)]
 pub enum InstructionType {
     SetInteger,
@@ -19,16 +20,26 @@ pub enum InstructionType {
     OpenClass
 }
 
+/// Struct for storing information about a single instruction.
 #[derive(Clone)]
 pub struct Instruction {
+    /// The type of instruction.
     pub instruction_type: InstructionType,
+
+    /// The arguments of the instruction.
     pub arguments: Vec<usize>,
+
+    /// The line from which the instruction originated.
     pub line: usize,
+
+    /// The column from which the instruction originated.
     pub column: usize
 }
 
 impl Instruction {
-    pub fn new(ins_type: InstructionType, arguments: Vec<usize>, line: usize, column: usize) -> Instruction {
+    /// Returns a new Instruction.
+    pub fn new(ins_type: InstructionType, arguments: Vec<usize>, line: usize,
+               column: usize) -> Instruction {
         Instruction {
             instruction_type: ins_type,
             arguments: arguments,
