e141475fc1d522408c6868322c162fda1c414344
Yorick Peterse <yorickpeterse@gmail.com>
2015-06-07 14:16:14 +0200

    Added tests for CallFrame

--- src/call_frame.rs
+++ src/call_frame.rs
@@ -83,3 +83,64 @@ impl CallFrame {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use compiled_code::CompiledCode;
+
+    #[test]
+    fn test_new() {
+        let frame = CallFrame
+            ::new("foo".to_string(), "test.aeon".to_string(), 1);
+
+        assert_eq!(frame.name, "foo".to_string());
+        assert_eq!(frame.file, "test.aeon".to_string());
+        assert_eq!(frame.line, 1);
+    }
+
+    #[test]
+    fn test_from_code() {
+        let code = CompiledCode
+            ::new("foo".to_string(), "test.aeon".to_string(), 1, vec![]);
+
+        let frame = CallFrame::from_code(&code);
+
+        assert_eq!(frame.name, "foo".to_string());
+        assert_eq!(frame.file, "test.aeon".to_string());
+        assert_eq!(frame.line, 1);
+    }
+
+    #[test]
+    fn test_set_parent() {
+        let frame1 = CallFrame
+            ::new("foo".to_string(), "test.aeon".to_string(), 1);
+
+        let mut frame2 = CallFrame
+            ::new("bar".to_string(), "baz.aeon".to_string(), 1);
+
+        frame2.set_parent(frame1);
+
+        assert!(frame2.parent.is_some());
+    }
+
+    #[test]
+    fn test_each_frame() {
+        let frame1 = CallFrame
+            ::new("foo".to_string(), "test.aeon".to_string(), 1);
+
+        let mut frame2 = CallFrame
+            ::new("bar".to_string(), "baz.aeon".to_string(), 1);
+
+        let mut names: Vec<String> = vec![];
+
+        frame2.set_parent(frame1);
+
+        frame2.each_frame(|frame| {
+            names.push(frame.name.clone());
+        });
+
+        assert_eq!(names[0], "bar".to_string());
+        assert_eq!(names[1], "foo".to_string());
+    }
+}
