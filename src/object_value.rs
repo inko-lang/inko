71bb7c470d094ca35c4d55ad74c4a382e49864f7
Yorick Peterse <yorickpeterse@gmail.com>
2015-09-15 20:09:30 +0200

    Use Box instead of *const u8 for object values
    This removes the need for unsafe code without increasing the size of the
    Object and ObjectValue structs.

--- src/object_value.rs
+++ src/object_value.rs
@@ -1,22 +1,15 @@
-use std::mem;
-
 use object::RcObject;
 use thread::RcThread;
-
-pub type ValuePointer = *const u8;
 
 /// Enum for storing different values in an Object.
 pub enum ObjectValue {
     None,
     Integer(isize),
     Float(f64),
-    ByteArray(ValuePointer),
-    Array(ValuePointer),
-    Thread(ValuePointer)
+    ByteArray(Box<Vec<u8>>),
+    Array(Box<Vec<RcObject>>),
+    Thread(RcThread)
 }
-
-unsafe impl Send for ObjectValue {}
-unsafe impl Sync for ObjectValue {}
 
 impl ObjectValue {
     pub fn is_integer(&self) -> bool {
@@ -37,8 +30,8 @@ impl ObjectValue {
 
     pub fn as_thread(&self) -> RcThread {
         match *self {
-            ObjectValue::Thread(val) => {
-                unsafe { mem::transmute::<ValuePointer, RcThread>(val).clone() }
+            ObjectValue::Thread(ref val) => {
+                val.clone()
             },
             _ => {
                 panic!("ObjectValue::as_thread() called on a non thread");
@@ -52,11 +45,7 @@ pub fn none() -> ObjectValue {
 }
 
 pub fn thread(value: RcThread) -> ObjectValue {
-    unsafe {
-        let pointer = mem::transmute::<RcThread, ValuePointer>(value);
-
-        ObjectValue::Thread(pointer)
-    }
+    ObjectValue::Thread(value)
 }
 
 pub fn integer(value: isize) -> ObjectValue {
@@ -69,21 +58,9 @@ pub fn float(value: f64) -> ObjectValue {
 
 // TODO: remove me once strings are just regular arrays of integers.
 pub fn byte_array(value: Vec<u8>) -> ObjectValue {
-    let boxed = Box::new(value);
-
-    unsafe {
-        let pointer = mem::transmute::<Box<Vec<u8>>, ValuePointer>(boxed);
-
-        ObjectValue::ByteArray(pointer)
-    }
+    ObjectValue::ByteArray(Box::new(value))
 }
 
 pub fn array(value: Vec<RcObject>) -> ObjectValue {
-    let boxed = Box::new(value);
-
-    unsafe {
-        let pointer = mem::transmute::<Box<Vec<RcObject>>, ValuePointer>(boxed);
-
-        ObjectValue::Array(pointer)
-    }
+    ObjectValue::Array(Box::new(value))
 }
