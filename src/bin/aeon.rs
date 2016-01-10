c57b0f7f9b88ea95a2a2026313f69737fca7233b
Yorick Peterse <yorickpeterse@gmail.com>
2016-01-10 21:48:20 +0100

    Added a basic CLI
    The CLI currently expects pre-compiled bytecode files instead of
    invoking the compiler itself.

--- src/bin/aeon.rs
+++ src/bin/aeon.rs
@@ -1,30 +1,70 @@
 extern crate libaeon;
+extern crate getopts;
 
-use std::sync::Arc;
+use std::io::prelude::*;
+use std::env;
+use std::fs::File;
 use std::process;
 
+use libaeon::bytecode_parser;
 use libaeon::virtual_machine::VirtualMachine;
 use libaeon::virtual_machine_methods::VirtualMachineMethods;
-use libaeon::compiled_code::CompiledCode;
-use libaeon::instruction::{InstructionType, Instruction};
+
+fn print_usage(options: &getopts::Options) {
+    println!("{}", options.usage("Usage: aeon FILE [OPTIONS]"));
+    process::exit(1);
+}
 
 fn main() {
-    let vm = VirtualMachine::new();
-    let mut cc = CompiledCode::new(
-        "<main>".to_string(),
-        "/tmp/test.rs".to_string(),
-        1,
-        vec![
-            Instruction::new(InstructionType::SetString, vec![0, 0], 1, 1),
-            Instruction::new(InstructionType::StdoutWrite, vec![1, 0], 1, 1)
-        ]
-    );
+    let args: Vec<String> = env::args().collect();
+    let mut options       = getopts::Options::new();
 
-    cc.add_string_literal("hello world\n".to_string());
+    options.optflag("h", "help", "Shows this help message");
+    options.optflag("v", "version", "Prints the version number");
 
-    let result = vm.start(Arc::new(cc));
+    let matches = match options.parse(&args[1..]) {
+        Ok(matches) => matches,
+        Err(error)  => panic!(error.to_string())
+    };
 
-    if result.is_err() {
-        process::exit(1);
+    if matches.opt_present("h") {
+        print_usage(&options);
+    }
+
+    if matches.opt_present("v") {
+        println!("aeon {}", env!("CARGO_PKG_VERSION"));
+        return;
+    }
+
+    if matches.free.is_empty() {
+        print_usage(&options);
+    }
+    else {
+        let ref path = matches.free[0];
+
+        match File::open(path) {
+            Ok(file) => {
+                let mut bytes = file.bytes();
+
+                match bytecode_parser::parse(&mut bytes) {
+                    Ok(code) => {
+                        let vm     = VirtualMachine::new();
+                        let status = vm.start(code);
+
+                        if status.is_err() {
+                            process::exit(1);
+                        }
+                    },
+                    Err(error) => {
+                        println!("Failed to parse file {}: {:?}", path, error);
+                        process::exit(1);
+                    }
+                }
+            },
+            Err(error) => {
+                println!("Failed to execute {}: {}", path, error.to_string());
+                process::exit(1);
+            }
+        }
     }
 }
