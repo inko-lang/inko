dad4ce3e63a554177292b67148f688e89c48625c
Yorick Peterse <yorickpeterse@gmail.com>
2016-03-06 23:08:34 +0100

    Added parsing functions for array/hash literals

--- src/parser.rs
+++ src/parser.rs
@@ -32,6 +32,16 @@ pub enum Node {
     },
     Expressions {
         children: Vec<Node>
+    },
+    Array {
+        values: Vec<Node>,
+        line: usize,
+        column: usize
+    },
+    Hash {
+        pairs: Vec<(Node, Node)>,
+        line: usize,
+        column: usize
     }
 }
 
@@ -50,7 +60,7 @@ pub fn parse(input: &str) -> ParserResult {
 }
 
 fn parse_expressions(lexer: &mut Lexer) -> ParserResult {
-    let mut nodes: Vec<Node> = Vec::new();
+    let mut nodes = Vec::new();
 
     loop {
         match parse_expression(lexer) {
@@ -70,10 +80,12 @@ fn parse_expressions(lexer: &mut Lexer) -> ParserResult {
 fn parse_expression(lexer: &mut Lexer) -> ParserResult {
     if let Some(token) = lexer.lex() {
         match token.kind {
-            TokenType::Integer => parse_integer(token),
-            TokenType::Float   => parse_float(token),
-            TokenType::String  => parse_string(token),
-            _                  => Err(ParserError::InvalidToken)
+            TokenType::Integer   => parse_integer(token),
+            TokenType::Float     => parse_float(token),
+            TokenType::String    => parse_string(token),
+            TokenType::BrackOpen => parse_array(token, lexer),
+            TokenType::CurlyOpen => parse_hash(token, lexer),
+            _                    => Err(ParserError::InvalidToken)
         }
     }
     else {
@@ -103,4 +115,52 @@ fn parse_string(token: Token) -> ParserResult {
     let value = token.value;
 
     Ok(Node::String { value: value, line: token.line, column: token.column })
+}
+
+fn parse_array(token: Token, lexer: &mut Lexer) -> ParserResult {
+    let mut values = Vec::new();
+
+    loop {
+        values.push(try!(parse_expression(lexer)));
+
+        if let Some(token) = lexer.lex() {
+            match token.kind {
+                TokenType::Comma      => {},
+                TokenType::BrackClose => break,
+                _                     => return Err(ParserError::InvalidToken)
+            };
+        }
+        else {
+            return Err(ParserError::EndOfInput);
+        }
+    }
+
+    Ok(Node::Array { values: values, line: token.line, column: token.column })
+}
+
+fn parse_hash(token: Token, lexer: &mut Lexer) -> ParserResult {
+    let mut pairs = Vec::new();
+
+    loop {
+        let key = try!(parse_expression(lexer));
+
+        next_token!(lexer, Colon);
+
+        let value = try!(parse_expression(lexer));
+
+        pairs.push((key, value));
+
+        if let Some(token) = lexer.lex() {
+            match token.kind {
+                TokenType::Comma      => {},
+                TokenType::CurlyClose => break,
+                _                     => return Err(ParserError::InvalidToken)
+            };
+        }
+        else {
+            return Err(ParserError::EndOfInput);
+        }
+    }
+
+    Ok(Node::Hash { pairs: pairs, line: token.line, column: token.column })
 }
