43adcbbfda468a49959c97e067d9933a1acc1e0f
Yorick Peterse <yorickpeterse@gmail.com>
2016-09-10 19:39:50 +0200

    Added tests for the Config struct

--- vm/src/config.rs
+++ vm/src/config.rs
@@ -1,10 +1,28 @@
+//! Virtual Machine Configuration
+//!
+//! Various virtual machine settings can be changed by the user, such as the
+//! directories to search for bytecode files and the number of operating system
+//! processes to use for running VM processes. These settings are stored in
+//! instances of the Config struct.
+
 use num_cpus;
 use std::path::PathBuf;
 
+/// Structure containing the configuration settings for the virtual machine.
 pub struct Config {
+    /// The directories to search in for extra bytecode files to run.
     pub directories: Vec<PathBuf>,
+
+    /// The number of operating system processes to use for running virtual
+    /// machine processes. Defaults to the number of CPU cores.
     pub process_threads: usize,
+
+    /// The number of garbage collector threads to run. Defaults to the number
+    /// of CPU cores.
     pub gc_threads: usize,
+
+    /// The number of reductions a process can perform before being suspended.
+    /// Defaults to 1000.
     pub reductions: usize,
 }
 
@@ -44,5 +62,56 @@ impl Config {
         if reductions > 0 {
             self.reductions = reductions;
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let config = Config::new();
+
+        assert_eq!(config.directories.len(), 0);
+        assert!(config.process_threads >= 1);
+        assert!(config.gc_threads >= 1);
+        assert_eq!(config.reductions, 1000);
+    }
+
+    #[test]
+    fn test_add_directory() {
+        let mut config = Config::new();
+
+        config.add_directory("foo".to_string());
+
+        assert_eq!(config.directories.len(), 1);
+    }
+
+    #[test]
+    fn test_set_process_threads() {
+        let mut config = Config::new();
+
+        config.set_process_threads(5);
+
+        assert_eq!(config.process_threads, 5);
+    }
+
+    #[test]
+    fn test_set_gc_threads() {
+        let mut config = Config::new();
+
+        config.set_gc_threads(5);
+
+        assert_eq!(config.gc_threads, 5);
+    }
+
+    #[test]
+    fn test_set_reductions() {
+        let mut config = Config::new();
+
+        config.set_reductions(5);
+
+        assert_eq!(config.reductions, 5);
     }
 }
