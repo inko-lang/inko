a67b82411ad1cbc9379e5bf003989e47cd5efcc1
Yorick Peterse <yorickpeterse@gmail.com>
2016-10-29 21:42:47 +0200

    Added tests for CopyObject
    This excludes File tests as this will soon be re-implemented using PIDs
    instead of directly operating on Rust File structures.

--- vm/src/immix/copy_object.rs
+++ vm/src/immix/copy_object.rs
@@ -65,3 +65,157 @@ pub trait CopyObject: Sized {
         self.allocate_copy(copy)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use immix::global_allocator::GlobalAllocator;
+    use immix::local_allocator::LocalAllocator;
+    use binding::Binding;
+    use compiled_code::CompiledCode;
+    use object::Object;
+    use object_pointer::ObjectPointer;
+    use object_value;
+
+    struct DummyAllocator {
+        pub allocator: LocalAllocator,
+    }
+
+    impl DummyAllocator {
+        pub fn new() -> DummyAllocator {
+            let global_alloc = GlobalAllocator::without_preallocated_blocks();
+
+            DummyAllocator { allocator: LocalAllocator::new(global_alloc) }
+        }
+    }
+
+    impl CopyObject for DummyAllocator {
+        fn allocate_copy(&mut self, object: Object) -> ObjectPointer {
+            self.allocator.allocate_copy(object)
+        }
+    }
+
+    #[test]
+    fn test_copy_none() {
+        let mut dummy = DummyAllocator::new();
+        let pointer = dummy.allocator.allocate_empty();
+        let copy = dummy.copy_object(pointer);
+
+        assert!(copy.get().value.is_none());
+    }
+
+    #[test]
+    fn test_copy_with_prototype() {
+        let mut dummy = DummyAllocator::new();
+        let pointer = dummy.allocator.allocate_empty();
+        let proto = dummy.allocator.allocate_empty();
+
+        pointer.get_mut().set_prototype(proto);
+
+        let copy = dummy.copy_object(pointer);
+
+        assert!(copy.get().prototype().is_some());
+    }
+
+    #[test]
+    fn test_copy_with_header() {
+        let mut dummy = DummyAllocator::new();
+        let ptr1 = dummy.allocator.allocate_empty();
+        let ptr2 = dummy.allocator.allocate_empty();
+
+        ptr1.get_mut().add_attribute("a".to_string(), ptr2);
+
+        let copy = dummy.copy_object(ptr1);
+
+        assert!(copy.get().header().is_some());
+    }
+
+    #[test]
+    fn test_copy_integer() {
+        let mut dummy = DummyAllocator::new();
+        let pointer = dummy.allocator
+            .allocate_without_prototype(object_value::integer(5));
+
+        let copy = dummy.copy_object(pointer);
+
+        assert!(copy.get().value.is_integer());
+        assert_eq!(copy.get().value.as_integer(), 5);
+    }
+
+    #[test]
+    fn test_copy_float() {
+        let mut dummy = DummyAllocator::new();
+        let pointer = dummy.allocator
+            .allocate_without_prototype(object_value::float(2.5));
+
+        let copy = dummy.copy_object(pointer);
+
+        assert!(copy.get().value.is_float());
+        assert_eq!(copy.get().value.as_float(), 2.5);
+    }
+
+    #[test]
+    fn test_copy_string() {
+        let mut dummy = DummyAllocator::new();
+        let pointer = dummy.allocator
+            .allocate_without_prototype(object_value::string("a".to_string()));
+
+        let copy = dummy.copy_object(pointer);
+
+        assert!(copy.get().value.is_string());
+        assert_eq!(copy.get().value.as_string(), &"a".to_string());
+    }
+
+    #[test]
+    fn test_copy_array() {
+        let mut dummy = DummyAllocator::new();
+        let ptr1 = dummy.allocator.allocate_empty();
+        let ptr2 = dummy.allocator.allocate_empty();
+        let array = dummy.allocator
+            .allocate_without_prototype(object_value::array(vec![ptr1, ptr2]));
+
+        let copy = dummy.copy_object(array);
+
+        assert!(copy.get().value.is_array());
+        assert_eq!(copy.get().value.as_array().len(), 2);
+    }
+
+    #[test]
+    fn test_copy_error() {
+        let mut dummy = DummyAllocator::new();
+        let ptr = dummy.allocator
+            .allocate_without_prototype(object_value::error(2));
+
+        let copy = dummy.copy_object(ptr);
+
+        assert!(copy.get().value.is_error());
+    }
+
+    #[test]
+    fn test_copy_compiled_code() {
+        let mut dummy = DummyAllocator::new();
+        let cc = CompiledCode::with_rc("a".to_string(),
+                                       "a".to_string(),
+                                       1,
+                                       Vec::new());
+
+        let ptr = dummy.allocator
+            .allocate_without_prototype(object_value::compiled_code(cc));
+
+        let copy = dummy.copy_object(ptr);
+
+        assert!(copy.get().value.is_compiled_code());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_copy_binding() {
+        let mut dummy = DummyAllocator::new();
+        let self_obj = dummy.allocator.allocate_empty();
+        let binding = Binding::new(self_obj);
+        let pointer = dummy.allocator
+            .allocate_without_prototype(object_value::binding(binding));
+
+        dummy.copy_object(pointer);
+    }
+}
