961579e0fe68f84d9ddf536313338947d269ecb1
Yorick Peterse <yorickpeterse@gmail.com>
2016-12-09 20:49:59 +0100

    Use a spinlock for the permanent allocator
    Fixes #52

--- vm/src/macros/objects.rs
+++ vm/src/macros/objects.rs
@@ -3,7 +3,7 @@
 macro_rules! copy_if_permanent {
     ($heap: expr, $source: expr, $dest: expr) => ({
         if $dest.is_permanent() {
-            write_lock!($heap).copy_object($source)
+            $heap.lock().copy_object($source)
         }
         else {
             $source
