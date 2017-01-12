6c1337968bf03c5cb2b57f41801911e900e06e99
Yorick Peterse <yorickpeterse@gmail.com>
2017-01-12 14:51:49 -0500

    Removed various unused macros

--- vm/src/macros/instructions.rs
+++
@@ -1,8 +0,0 @@
-#![macro_use]
-
-/// Calls an instruction method on a given receiver.
-macro_rules! run {
-    ($rec: expr, $name: ident, $process: ident, $code: ident, $ins: ident) => (
-        try!($rec.$name($process.clone(), $code.clone(), &$ins))
-    );
-}
--- vm/src/macros/mod.rs
+++ vm/src/macros/mod.rs
@@ -1,9 +1,6 @@
 #![macro_use]
 
-pub mod checks;
 pub mod error_handling;
-pub mod files;
 pub mod locking;
-pub mod instructions;
 pub mod numeric_operations;
 pub mod objects;
