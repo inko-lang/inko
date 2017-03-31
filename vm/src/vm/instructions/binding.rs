bb30743f8166964d5ccc3061138f739805ccef5d
Yorick Peterse <yorickpeterse@gmail.com>
2017-04-01 01:43:32 +0200

    Remove binding_of_caller instruction

--- vm/src/vm/instructions/binding.rs
+++ vm/src/vm/instructions/binding.rs
@@ -27,37 +27,3 @@ pub fn get_binding(machine: &Machine,
 
     Ok(Action::None)
 }
-
-/// Gets the binding of a caller.
-///
-/// If no binding could be found the current binding is returned instead.
-///
-/// This instruction requires two arguments:
-///
-/// 1. The register to store the binding object in.
-/// 2. The register containing the number of bindings to walk upwards as an
-///    integer
-pub fn get_binding_of_caller(machine: &Machine,
-                             process: &RcProcess,
-                             _: &RcCompiledCode,
-                             instruction: &Instruction)
-                             -> InstructionResult {
-    let register = instruction.arg(0)?;
-    let depth_ptr = process.get_register(instruction.arg(1)?)?;
-    let start_context = process.context();
-
-    let depth = depth_ptr.integer_value()? as usize;
-
-    let binding = if let Some(context) = start_context.find_parent(depth) {
-        context.binding()
-    } else {
-        start_context.binding()
-    };
-
-    let obj = process.allocate(object_value::binding(binding),
-                               machine.state.binding_prototype.clone());
-
-    process.set_register(register, obj);
-
-    Ok(Action::None)
-}
