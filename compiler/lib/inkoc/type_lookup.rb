9aa965324fcc12b2cc2b12954dc658a7df2aff3a
Yorick Peterse <yorickpeterse@gmail.com>
2017-09-25 22:51:09 +0200

    Remove #initialize_type

--- compiler/lib/inkoc/type_lookup.rb
+++ compiler/lib/inkoc/type_lookup.rb
@@ -20,29 +20,5 @@ module Inkoc
 
       Type::Dynamic.new
     end
-
-    def initialize_type(type, node, sources)
-      return type if type.type_parameter?
-
-      exp_len = type.type_parameters.length
-      got_len = node.type_parameters.length
-      instance = type.new_instance
-
-      if exp_len != got_len
-        diagnostics.type_parameters_error(exp_len, got_len, node.location)
-
-        return Type::Dynamic.new
-      end
-
-      param_names = type.type_parameter_names
-
-      node.type_parameters.each_with_index do |arg, index|
-        arg_type = type_for_constant(arg, sources)
-
-        instance.init_type_parameter_by_name(param_names[index], arg_type)
-      end
-
-      instance
-    end
   end
 end
