9aad679a692a07c2be8a08b67bd966ed52e92df9
Yorick Peterse <yorickpeterse@gmail.com>
2018-08-11 23:23:45 +0200

    Don't use grep -oP in the arch detection script
    This makes it work on Mac OS / BSD, where -o and -P might not be
    supported by default.

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -2,14 +2,18 @@
 
 set -e
 
+function rustc_cfg() {
+    rustc --print cfg | grep "$1" | cut -d '=' -f 2 | cut -d '"' -f 2
+}
+
 function print-arch {
     local os
     local arch
     local env
 
-    os="$(rustc --print cfg | grep -oP 'target_os="\K(\w+)(?=")')"
-    arch="$(rustc --print cfg | grep -oP 'target_arch="\K(\w+)(?=")')"
-    env="$(rustc --print cfg | grep -oP 'target_env="\K(\w+)(?=")')"
+    os="$(rustc_cfg target_os)"
+    arch="$(rustc_cfg target_arch)"
+    env="$(rustc_cfg target_env)"
 
     echo "${arch}-${os}-${env}"
 }
