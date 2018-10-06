8be5d5b3169e6c03aa73e5eef7bbdb5260239bd3
Yorick Peterse <yorickpeterse@gmail.com>
2018-10-06 17:27:53 +0200

    Use Chunk instead of Vec in Histogram
    The use of a Chunk instead of Vec reduces the type size of Process by 96
    bytes. We don't need Vec anyway, since a Histogram always has a fixed
    size.

--- vm/src/immix/histogram.rs
+++ vm/src/immix/histogram.rs
@@ -6,12 +6,13 @@
 //!
 //! Histograms are of a fixed size and use atomic operations for incrementing
 //! bucket values, allowing concurrent use of the same histogram.
+use chunk::Chunk;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 const DEFAULT_VALUE: usize = 0;
 
 pub struct Histogram {
-    values: Vec<AtomicUsize>,
+    values: Chunk<AtomicUsize>,
 }
 
 /// Iterator for traversing the most fragmented bins in a histogram.
@@ -22,29 +23,35 @@ pub struct HistogramIterator<'a> {
 
 impl Histogram {
     pub fn new(capacity: usize) -> Self {
-        let mut vec = Vec::with_capacity(capacity);
+        let mut chunk = Chunk::new(capacity);
 
-        for index in 0..vec.capacity() {
-            vec.insert(index, AtomicUsize::new(DEFAULT_VALUE));
+        for index in 0..capacity {
+            chunk[index] = AtomicUsize::new(DEFAULT_VALUE);
         }
 
-        Histogram { values: vec }
+        Histogram { values: chunk }
     }
 
     /// Increments a bin by the given value.
+    ///
+    /// Bounds checking is not performed, as the garbage collector never uses an
+    /// out of bounds index.
     pub fn increment(&self, index: usize, value: usize) {
         self.values[index].fetch_add(value, Ordering::Release);
     }
 
     /// Returns the value for the given bin.
+    ///
+    /// Bounds checking is not performed, as the garbage collector never uses an
+    /// out of bounds index.
     pub fn get(&self, index: usize) -> usize {
         self.values[index].load(Ordering::Acquire)
     }
 
     /// Returns the most fragmented bin.
     pub fn most_fragmented_bin(&self) -> usize {
-        for (bin, value) in self.values.iter().enumerate().rev() {
-            if value.load(Ordering::Acquire) > DEFAULT_VALUE {
+        for bin in (0..self.values.len()).rev() {
+            if self.values[bin].load(Ordering::Acquire) > DEFAULT_VALUE {
                 return bin;
             }
         }
@@ -93,7 +100,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_increment_within_bounds() {
+    fn test_increment() {
         let histo = Histogram::new(1);
 
         histo.increment(0, 10);
@@ -102,21 +109,13 @@ mod tests {
     }
 
     #[test]
-    fn test_increment_successive_within_bounds() {
+    fn test_increment_successive() {
         let histo = Histogram::new(1);
 
         histo.increment(0, 5);
         histo.increment(0, 5);
 
         assert_eq!(histo.get(0), 10);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_increment_out_of_bounds() {
-        let histo = Histogram::new(1);
-
-        histo.increment(2, 10);
     }
 
     #[test]
