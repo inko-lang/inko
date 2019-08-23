460e66b5f96a9812567a425fec2e0b9c2de68302
Yorick Peterse <yorickpeterse@gmail.com>
2019-08-24 00:34:32 +0200

    Reduce use of format!() for VM panics
    This reduces the binary size a little bit (about 600 bytes). This is not
    much, but the use of format!() in these cases was overkill regardless of
    the binary size.

--- vm/src/runtime_panic.rs
+++ vm/src/runtime_panic.rs
@@ -6,10 +6,10 @@ pub fn display_panic(process: &RcProcess, message: &str) {
 
     for context in process.context().contexts() {
         frames.push(format!(
-            "{}, line {}, in {}",
-            format!("{:?}", context.code.file.string_value().unwrap()),
+            "\"{}\", line {}, in \"{}\"",
+            context.code.file.string_value().unwrap(),
             context.line.to_string(),
-            format!("{:?}", context.code.name.string_value().unwrap())
+            context.code.name.string_value().unwrap()
         ));
     }
 
