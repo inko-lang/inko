42bc9b0ba5c319c21d54965b61c21d0cbd12e59d
Yorick Peterse <yorick@yorickpeterse.com>
2020-01-05 03:16:59 +0100

    Don't define a panic handler in std::vm
    By just using the VM's built-in panic handler (which now produces the
    same output) we can simplify std::vm, and we no longer need to import it
    in std::prelude.

--- vm/src/runtime_panic.rs
+++ vm/src/runtime_panic.rs
@@ -3,10 +3,11 @@ use crate::process::RcProcess;
 /// Prints a runtime panic to STDERR.
 pub fn display_panic(process: &RcProcess, message: &str) {
     let mut frames = Vec::new();
+    let mut buffer = String::new();
 
     for context in process.context().contexts() {
         frames.push(format!(
-            "\"{}\", line {}, in \"{}\"",
+            "\"{}\" line {}, in \"{}\"",
             context.code.file.string_value().unwrap(),
             context.line.to_string(),
             context.code.name.string_value().unwrap()
@@ -15,13 +16,17 @@ pub fn display_panic(process: &RcProcess, message: &str) {
 
     frames.reverse();
 
-    eprintln!("Stack trace (the most recent call comes last):");
-
-    let index_padding = frames.len().to_string().len();
+    buffer.push_str("Stack trace (the most recent call comes last):");
 
     for (index, line) in frames.iter().enumerate() {
-        eprintln!("  {}: {}", format!("{:01$}", index, index_padding), line);
+        buffer.push_str(&format!("\n  {}: {}", index, line));
     }
 
-    eprintln!("Process {:#x} panicked: {}", process.identifier(), message);
+    buffer.push_str(&format!(
+        "\nProcess {:#x} panicked: {}",
+        process.identifier(),
+        message
+    ));
+
+    eprintln!("{}", buffer);
 }
