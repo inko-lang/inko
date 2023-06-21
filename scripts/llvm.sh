ae8f488181fc5b3638a5295211786fc5a6942b3f
Yorick Peterse <git@yorickpeterse.com>
2023-06-21 03:31:08 +0200

    Run apt-get update before installing LLVM

--- scripts/llvm.sh
+++ scripts/llvm.sh
@@ -6,6 +6,7 @@ if [ "$RUNNER_OS" = "Linux" ]
 then
     # libclang-common is needed because of
     # https://gitlab.com/taricorp/llvm-sys.rs/-/issues/16.
+    sudo apt-get update
     sudo apt-get install --yes llvm-15 llvm-15-dev libstdc++-11-dev \
         libclang-common-15-dev zlib1g-dev
 elif [ "$RUNNER_OS" = "macOS" ]
