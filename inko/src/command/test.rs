da6ea1c31150b92aa0094de0c8417875e24ac32f
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-11-15 20:25:38 +0100

    Automatically generate main file for unit tests
    This commit implements automatic generation of the main file
    (inko-tests.inko) for running unit tests. This file is placed
    in the build/ directory and is regenerated on each run. The file
    is not removed after the tests are run to allow inspection.
    
    Changelog: added

--- inko/src/command/test.rs
+++ inko/src/command/test.rs
@@ -1,9 +1,12 @@
 use crate::error::Error;
 use crate::options::print_usage;
 use compiler::compiler::{CompileError, Compiler};
-use compiler::config::{Config, Output};
+use compiler::config::{Config, Output, SOURCE_EXT};
 use getopts::Options;
+use std::fs::{read_dir, write};
+use std::path::{Path, PathBuf};
 use std::process::Command;
+use types::module_name::ModuleName;
 
 const USAGE: &str = "Usage: inko test [OPTIONS]
 
@@ -42,7 +45,20 @@ pub(crate) fn run(arguments: &[String]) -> Result<i32, Error> {
     config.add_source_directory(config.tests.clone());
     config.output = Output::File("inko-tests".to_string());
 
+    let tests = test_module_names(&config.tests).map_err(|err| {
+        Error::generic(format!("Failed to find test modules: {}", err))
+    })?;
+
     let mut compiler = Compiler::new(config);
+
+    // The build/ directory needs to be created first, otherwise we can't save
+    // the generated file in it (if it doesn't already exist that is).
+    compiler.create_build_directory()?;
+
+    write(&input, generate_main_test_module(tests)).map_err(|err| {
+        Error::generic(format!("Failed to write {}: {}", input.display(), err))
+    })?;
+
     let result = compiler.build(Some(input));
 
     compiler.print_diagnostics();
@@ -59,4 +75,84 @@ pub(crate) fn run(arguments: &[String]) -> Result<i32, Error> {
         Err(CompileError::Invalid) => Ok(1),
         Err(CompileError::Internal(msg)) => Err(Error::generic(msg)),
     }
+}
+
+fn is_test_file(path: &Path) -> bool {
+    match path.extension().and_then(|p| p.to_str()) {
+        Some(SOURCE_EXT) if path.is_file() => {}
+        _ => return false,
+    }
+
+    path.file_name()
+        .map(|v| v.to_string_lossy())
+        .map_or(false, |v| v.starts_with("test_"))
+}
+
+fn test_files(test_dir: &Path) -> Result<Vec<PathBuf>, std::io::Error> {
+    let mut found = Vec::new();
+    let mut pending = vec![test_dir.to_owned()];
+
+    while let Some(path) = pending.pop() {
+        let entries = read_dir(&path)?;
+
+        for entry in entries {
+            let entry = entry?;
+            let path = entry.path();
+
+            if path.is_dir() {
+                pending.push(path);
+                continue;
+            }
+
+            if is_test_file(&path) {
+                found.push(path);
+            }
+        }
+    }
+
+    Ok(found)
+}
+
+fn test_module_names(
+    test_dir: &Path,
+) -> Result<Vec<ModuleName>, std::io::Error> {
+    let test_modules = test_files(test_dir)?
+        .into_iter()
+        .map(|file| {
+            ModuleName::from_relative_path(file.strip_prefix(test_dir).unwrap())
+        })
+        .collect::<Vec<_>>();
+
+    Ok(test_modules)
+}
+
+fn generate_main_test_module(tests: Vec<ModuleName>) -> String {
+    let mut imports = Vec::with_capacity(tests.len());
+    let mut calls = Vec::with_capacity(tests.len());
+
+    for (idx, test) in tests.iter().enumerate() {
+        imports.push(format!("import {}.(self as tests{})\n", test, idx));
+        calls.push(format!("    tests{}.tests(tests)\n", idx));
+    }
+
+    let mut source =
+        "import std.env\nimport std.test.(Filter, Tests)\n".to_string();
+
+    for line in imports {
+        source.push_str(&line);
+    }
+
+    source.push_str("\nclass async Main {\n");
+    source.push_str("  fn async main {\n");
+    source.push_str("    let tests = Tests.new\n\n");
+
+    for line in calls {
+        source.push_str(&line);
+    }
+
+    source.push_str("    tests.filter = Filter.from_string(env.arguments.opt(0).unwrap_or(''))\n");
+    source.push_str("    tests.run\n");
+    source.push_str("  }\n");
+    source.push_str("}\n");
+    source
 }
