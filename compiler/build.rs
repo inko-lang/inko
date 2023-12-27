fd0a0b9bf57be8251753a86c9d86657563e5e361
Yorick Peterse <git@yorickpeterse.com>
2023-12-27 02:59:34 +0100

    Fix build script for custom profile names
    This ensures that when using custom profile names, such as the
    release-debug profile, the linker can still find the runtime library.
    
    Changelog: fixed

--- compiler/build.rs
+++ compiler/build.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::path::PathBuf;
+use std::path::{PathBuf, MAIN_SEPARATOR};
 
 fn main() {
     // To make development easier we default to using repository-local paths.
@@ -33,11 +33,12 @@ fn std_directory() -> PathBuf {
 
 fn runtime_directory() -> PathBuf {
     let target = env::var("TARGET").unwrap();
-    let profile = env::var("PROFILE").unwrap();
+    let profile = profile_name();
     let base = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
         .parent()
         .unwrap()
         .join("target");
+
     let without_target = base.join(&profile);
     let with_target = base.join(target).join(profile);
 
@@ -46,4 +47,20 @@ fn runtime_directory() -> PathBuf {
     } else {
         without_target
     }
+}
+
+/// Returns the name of the profile used, taking into account custom profile
+/// names.
+///
+/// See the following issues for more details:
+///
+/// - https://stackoverflow.com/questions/73595435/how-to-get-profile-from-cargo-toml-in-build-rs-or-at-runtime
+/// - https://github.com/rust-lang/cargo/issues/9661
+fn profile_name() -> String {
+    env::var("OUT_DIR")
+        .unwrap()
+        .split(MAIN_SEPARATOR)
+        .nth_back(3)
+        .map(|v| v.to_string())
+        .unwrap_or_else(|| env::var("PROFILE").unwrap())
 }
