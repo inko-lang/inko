efba0cf6195b55ebffb32b02d83b374f49636c01
Yorick Peterse <git@yorickpeterse.com>
2024-07-04 00:19:12 +0800

    Make copying code blocks easier
    When hovering over code blocks, we now show a copy button. When clicked,
    the textual representation of a code block is copied to the clipboard.

--- docs/source/js/main.js
+++ docs/source/js/main.js
@@ -1,9 +1,9 @@
 (function() {
   "use strict";
 
-  document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.expand-menus a').forEach(function(button) {
-      button.addEventListener('click', function(event) {
+  document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.expand-menus a').forEach((button) => {
+      button.addEventListener('click', (event) => {
         event.preventDefault();
 
         let query = button.dataset.toggle;
@@ -13,6 +13,23 @@
         button.dataset.toggleText = old_text;
         button.innerText = new_text;
         document.querySelector(query).classList.toggle('visible');
+      });
+    });
+
+    document.querySelectorAll('.highlight .copy').forEach((el) => {
+      let timer = null;
+
+      el.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (timer) {
+          clearTimeout(timer);
+          timer = null;
+        }
+
+        el.classList.add('copied');
+        navigator.clipboard.writeText(el.previousSibling.textContent);
+        timer = setTimeout(function() { el.classList.remove('copied'); }, 2000);
       });
     });
   });
