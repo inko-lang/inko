e93cb49313a734f8eb0c8389e6b708d091bb9afe
Yorick Peterse <yorick@yorickpeterse.com>
2024-09-05 18:41:14 +0200

    Use Inko for getting the errno location
    We can just read this using the C FFI instead of going through an extra
    Rust function call.
    
    Changelog: changed

--- rt/src/runtime/general.rs
+++ rt/src/runtime/general.rs
@@ -2,28 +2,6 @@ use crate::mem::{header_of, ClassPointer};
 use crate::process::ProcessPointer;
 use crate::runtime::process::panic;
 use std::alloc::handle_alloc_error;
-use std::io::Error;
-
-// Taken from Rust's standard library, with some removals of platforms we don't
-// support.
-extern "C" {
-    #[cfg_attr(any(target_os = "linux",), link_name = "__errno_location")]
-    #[cfg_attr(
-        any(target_os = "netbsd", target_os = "android",),
-        link_name = "__errno"
-    )]
-    #[cfg_attr(
-        any(
-            target_os = "macos",
-            target_os = "ios",
-            target_os = "tvos",
-            target_os = "freebsd",
-            target_os = "watchos"
-        ),
-        link_name = "__error"
-    )]
-    fn errno_location() -> *mut i32;
-}
 
 #[no_mangle]
 pub unsafe extern "system" fn inko_reference_count_error(
@@ -44,14 +22,4 @@ pub unsafe extern "system" fn inko_reference_count_error(
 #[no_mangle]
 pub unsafe extern "system" fn inko_alloc_error(class: ClassPointer) -> ! {
     handle_alloc_error(class.instance_layout());
-}
-
-#[no_mangle]
-pub unsafe extern "system" fn inko_last_error() -> i64 {
-    Error::last_os_error().raw_os_error().unwrap_or(-1) as i64
-}
-
-#[no_mangle]
-pub unsafe extern "system" fn inko_reset_error() {
-    *errno_location() = 0;
 }
