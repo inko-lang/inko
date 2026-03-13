8ebcc1632d683285b3944f26e78494d63da1981c
Yorick Peterse <yorick@yorickpeterse.com>
2026-03-13 16:11:36 +0100

    Disable rclone checksumming
    This isn't really necessary and it puts quite a bit of stress on the
    server when multiple deployments are running in parallel.

--- scripts/rclone.sh
+++ scripts/rclone.sh
@@ -11,7 +11,6 @@ run() {
         --multi-thread-streams 8 \
         --transfers 8 \
         --metadata \
-        --checksum \
         --no-update-dir-modtime \
         --sftp-host "${SERVER}" \
         --sftp-user "${USER}" \
